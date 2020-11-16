e74efe25adf938523cee8a4d5c190bd76b391f85
Marco Israel Silicaro <marcoisraelsilicaro@gmail.com>
2020-11-16 09:23:59 -0300

    barebones

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div class='home__container2'>
+            <div class='home__container'>
+                <div class='home__sidebar'>
+                    Sidebar
+                </div>
+                <div class='home__chat'>
+                    Chatt
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
